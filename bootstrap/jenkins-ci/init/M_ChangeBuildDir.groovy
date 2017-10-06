de97efd9ae5da3e9429aeca5d1a33a4292158441
Alan <alan@codecrunchers.ie>
2017-10-06 01:16:55 +0100

    separate docker images

--- bootstrap/jenkins-ci/init/M_ChangeBuildDir.groovy
+++
@@ -1,8 +0,0 @@
-println("-------> Point Build and Workspace at Host Disk instead of EFS")
-import hudson.Util;
-import jenkins.model.Jenkins;
-import java.io.File;
-import java.io.IOException;
-
-String storedConfig = Util.loadFile(new File(Jenkins.getInstance().getRootDir(),"config.xml"));
-
--- bootstrap/jenkins-ci/init/README.md
+++
@@ -1,4 +0,0 @@
-# Jenkins Init Scripts - anything here will be run on boot - every time!
-Run in ArrayList.sort()'d order
-
-
